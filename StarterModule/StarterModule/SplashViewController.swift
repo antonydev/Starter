1c59b3c57aa907757f81b71db7e490c9e1a5af87
AntonyOuseph <antony.k@foundingminds.com>
2016-08-16 16:52:48 +0530

    Initial commit
    1. App Icons.
    2. Splash screens.

---
+++ StarterModule/StarterModule/SplashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplashViewController.swift
+//  StarterModule
+//
+//  Created by antony on 8/16/16.
+//  Copyright © 2016 antony. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
